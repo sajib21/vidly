b19dca23d5e5f9629e473de9e084b52ffe807e3d
sajib21 <shariarsajib@gmail.com>
2020-06-20 18:48:43 +0600

    async and error middleware to handle db exceptions

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
 const auth = require("./routes/auth");
+const error = require("./middleware/error");
 
 const app = express();
 
@@ -24,6 +25,8 @@ app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+
+app.use(error);
 
 const port = 8000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
